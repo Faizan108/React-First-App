31cbd221a3ed398bbe5fe32f4c24ac4bb0c63f4c
Faizan Saifi <faizan.19b121056@abes.ac.in>
2022-01-26 22:29:43 +0530

    added more components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
  const expense=[
@@ -31,11 +32,8 @@ function App() {
   ]
   return (
     <div className="App-header">
-     <h1>Let's Get Started</h1>
-     <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-     <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
-     <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
-     <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date}></ExpenseItem>
+     <h1 className="App">Let's Get Started</h1>
+     <Expenses items={expense}></Expenses>
    </div>
   );
 }
