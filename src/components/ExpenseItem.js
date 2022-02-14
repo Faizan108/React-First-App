26ad1e130642bf79078d9efa9c57725425e3315b
Faizan Saifi <saniasaifi108@gmail.com>
2022-02-14 23:24:46 +0530

    added some buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,21 @@
 import './ExpenseItem.css';
+import React ,{useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) =>{   
+        const [title,setTitle] = useState(props.title);
+        const clickhandler =() =>{
+            setTitle('Updated!')
+            console.log('Clicked');
+        }
     return (
         <div className="expense-item">
            <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickhandler}>Click on me</button>
         </div>
     );
 }
