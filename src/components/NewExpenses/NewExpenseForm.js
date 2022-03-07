1d05161c5191fcc249fd90c58e8b291353420fb6
Faizan Saifi <saniasaifi108@gmail.com>
2022-03-07 20:16:31 +0530

    Added new states

--- src/components/NewExpenses/NewExpenseForm.js
+++ src/components/NewExpenses/NewExpenseForm.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React,{useState} from "react";
 import './NewExpenseForm.css';
 
 const NewExpenseForm = () =>{
+    // const [enteredtitle, setTitle]=useState('');
+    // const [enteredAmount, setAmount]=useState('');
+    // const [enteredDate, setDate]=useState('');
+    const [userInput, setuserInput] = useState({
+        enteredTitle:'',
+        enteredAmount:'',
+        enteredDate:'',
+    });
+    const titleclickhandler = (event) =>{
+        setuserInput((prevState) =>{
+            return {
+                ...prevState, enteredTitle: event.target.value
+            };
+        });
+        
+    }
+    const amountclickhandler = (event) =>{
+       setUserInput((prevState) => {
+        return {
+                ...prevState, enteredAmount: event.target.value
+        };
+       });
+        
+    }
+    const dateclickhandler = (event) =>{
+       setUserInput((prevState) => {
+        return {...prevState, enteredDate: event.target.value};
+       });
+       
+        
+    }
     return(
         <form>
         <div class="new-expense__controls">
             <div class="new-expense__control">
                 <label>Title</label>
-                <input type="text"></input>
+                <input type="text" onChange={titleclickhandler}></input>
             </div>
             <div class="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"></input>
+                <input type="number" min="0.01" step="0.01" onChange={amountclickhandler}></input>
             </div>
             <div class="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2024-12-30"></input>
+                <input type="date" min="2019-01-01" max="2024-12-30" onchange={dateclickhandler}></input>
             </div>
-            
+
         </div>
         <div class="new-expense__actions">
                 <button type="submit">Add Expense</button>
